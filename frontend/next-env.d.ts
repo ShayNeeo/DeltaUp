496d36a195be6af5357e1e96a21e55d88948fb80
ShayNeeo <phamquocthanh357@gmail.com>
2025-11-09 23:41:14 +0700

    fix

---
+++ frontend/next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited as it can cause unexpected behavior in your application.
+// See https://nextjs.org/docs/basic-features/typescript for more information.
+
