21362e02de83c86276047c7c9a1fef786430a89f
ShayNeeo <phamquocthanh357@gmail.com>
2025-11-10 01:23:44 +0700

    fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -14,6 +13,4 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-
-export default config;
+}
